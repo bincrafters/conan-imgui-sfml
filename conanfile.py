7d2f0fbfc48aaf907c143a64fe929aa84123d42a
Croydon <git@cr0ydon.com>
2020-02-16 10:57:38 +0100

    Update conventions
    * patch files should be in patches/ and listed in conandata.yml
    
    * default find/config CMake files should not be packaged unless they have important macros/functions which can't be automatical generated by Conan generators
    
    * all source downloads must be listed under one version key in the conandata.yml file
    
    * if it is actually desired to make the imgui version configurable, all possible versions must be listed under the same version key in conandata.yml. It should work to iterate over all downloads and pick the right download by some name matching. Possible values should then be also explicitly listed as valid values for the imgui_version Conan option. ANY should be avoided if possible

--- conanfile.py
+++ conanfile.py
@@ -1,36 +1,34 @@
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 import os.path
+import glob
 
 
 class ImguiSfmlConan(ConanFile):
     name = "imgui-sfml"
     version = "2.1"
-    description = 'ImGui binding for use with SFML'
-    topics = ('conan', 'sfml', 'gui', 'imgui')
+    description = "ImGui binding for use with SFML"
+    topics = ("conan", "sfml", "gui", "imgui")
     url = "https://github.com/bincrafters/conan-imgui-sfml"
-    homepage = 'https://github.com/eliasdaler/imgui-sfml'
+    homepage = "https://github.com/bincrafters/imgui-sfml"
     license = "MIT"
     generators = "cmake"
-    exports_sources = ['CMakeLists.txt', '0001-conan-libs.patch']
-    exports = ['LICENSE.md']
+    exports_sources = ["CMakeLists.txt", "patches/*"]
 
-    settings = 'os', 'compiler', 'build_type', 'arch'
+    settings = "os", "compiler", "build_type", "arch"
     options = {
-        'shared': [True, False],
-        'fPIC': [True, False],
-        'imconfig': 'ANY',
-        'imconfig_install_folder': 'ANY',
-        'imgui_version': 'ANY'
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "imconfig": "ANY",
+        "imconfig_install_folder": "ANY",
     }
     default_options = {
-        'shared': False,
-        'fPIC': True,
-        'imconfig': None,
-        'imconfig_install_folder': None,
-        'imgui_version': '1.75',
-        'sfml:window': True,
-        'sfml:graphics': True
+        "shared": False,
+        "fPIC": True,
+        "imconfig": None,
+        "imconfig_install_folder": None,
+        "sfml:window": True,
+        "sfml:graphics": True
     }
 
     _source_subfolder = "source_subfolder"
@@ -39,11 +37,11 @@ class ImguiSfmlConan(ConanFile):
     _imconfig_path = ""
 
     requires = (
-        'sfml/2.5.1@bincrafters/stable'
+        "sfml/2.5.1@bincrafters/stable"
     )
 
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
         imconfig = self.options.imconfig
         if imconfig:
@@ -51,41 +49,48 @@ class ImguiSfmlConan(ConanFile):
                 raise ConanInvalidConfiguration("Provided user config is not a file or doesn't exist")
             else:
                 self._imconfig_path = os.path.abspath(str(self.options.imconfig))
-        if not self.options.imgui_version:
-            raise ConanInvalidConfiguration("ImGui version is empty")
 
     def configure(self):
-        self.options['sfml'].shared = self.options.shared
+        self.options["sfml"].shared = self.options.shared
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
+        tools.get(**self.conan_data["sources"][self.version][0])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-        tools.get(**self.conan_data["sources"]["imgui-{}".format(self.options.imgui_version)])
-        extracted_dir = "imgui-{}".format(self.options.imgui_version)
+        tools.get(**self.conan_data["sources"][self.version][1])
+        extracted_dir = glob.glob("imgui-*")[0]
         os.rename(extracted_dir, self._imgui_subfolder)
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions['IMGUI_DIR'] = os.path.join(self.source_folder, self._imgui_subfolder)
-        cmake.definitions['SFML_DIR'] = os.path.join(self.deps_cpp_info['sfml'].lib_paths[0], 'cmake', 'SFML')
-        cmake.definitions['IMGUI_SFML_BUILD_EXAMPLES'] = 'OFF'
-        cmake.definitions['IMGUI_SFML_FIND_SFML'] = 'OFF'
+        cmake.definitions["IMGUI_DIR"] = os.path.join(self.source_folder, self._imgui_subfolder)
+
+        # FIXME: Shipping the default config/find CMake files is against (newer) Conan conventions
+        # Packages should be independent of specific build systems for customers
+        # This SFML/cmake path is becoming eventually invalid
+        # Try to migrate to one of the Conan CMake generators like cmake_find_package_multi
+        cmake.definitions["SFML_DIR"] = os.path.join(self.deps_cpp_info["sfml"].lib_paths[0], "cmake", "SFML")
+
+        cmake.definitions["IMGUI_SFML_BUILD_EXAMPLES"] = "OFF"
+        cmake.definitions["IMGUI_SFML_FIND_SFML"] = "OFF"
         if self.options.imconfig_install_folder:
-            cmake.definitions['IMGUI_SFML_CONFIG_INSTALL_DIR'] = self.options.imconfig_install_folder
+            cmake.definitions["IMGUI_SFML_CONFIG_INSTALL_DIR"] = self.options.imconfig_install_folder
         if self.options.imconfig:
-            cmake.definitions['IMGUI_SFML_USE_DEFAULT_CONFIG'] = 'OFF'
-            cmake.definitions['IMGUI_SFML_CONFIG_NAME'] = os.path.basename(self._imconfig_path)
-            cmake.definitions['IMGUI_SFML_CONFIG_DIR'] = os.path.dirname(self._imconfig_path)
+            cmake.definitions["IMGUI_SFML_USE_DEFAULT_CONFIG"] = "OFF"
+            cmake.definitions["IMGUI_SFML_CONFIG_NAME"] = os.path.basename(self._imconfig_path)
+            cmake.definitions["IMGUI_SFML_CONFIG_DIR"] = os.path.dirname(self._imconfig_path)
         else:
-            cmake.definitions['IMGUI_SFML_USE_DEFAULT_CONFIG'] = 'ON'
+            cmake.definitions["IMGUI_SFML_USE_DEFAULT_CONFIG"] = "ON"
 
         cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        tools.patch(self._source_subfolder, patch_file="0001-conan-libs.patch")
+        if "patches" in self.conan_data and self.version in self.conan_data["patches"]:
+            for patch in self.conan_data["patches"][self.version]:
+                tools.patch(**patch)
+
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -94,5 +99,9 @@ class ImguiSfmlConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
 
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.names["cmake_find_package"] = "ImGui-SFML"
+        self.cpp_info.names["cmake_find_package_multi"] = "ImGui-SFML"
